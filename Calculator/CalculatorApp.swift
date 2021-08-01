e27a8d0f9e9ddfd01d9a9991f57da433ba7185ae
Sankew <sankeshwarsivakumar@gmail.com>
2021-08-01 15:15:29 +0400

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Sankeshwar Sivakumar  on 01/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
